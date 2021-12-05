ba3e0f9e2c3dbcb05a7c82b0bd0888961ea59709
tsksandeep <tsksandeep11@gmail.com>
2021-12-06 01:49:38 +0530

    Updated configs

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,14 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCf9JsUd7uSedFT9dOb63J5q1ZubmGx9uU",
-  authDomain: "thagaval-23229.firebaseapp.com",
-  databaseURL:
-    "https://thagaval-23229-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "thagaval-23229",
-  storageBucket: "thagaval-23229.appspot.com",
-  messagingSenderId: "456919165595",
-  appId: "1:456919165595:web:5b3303c267eba8a741f43e",
+  apiKey: "AIzaSyAZcO7i_YGU34iktBMlZYRnwUTlJHCjKTo",
+  authDomain: "thagaval-dbd6f.firebaseapp.com",
+  projectId: "thagaval-dbd6f",
+  storageBucket: "thagaval-dbd6f.appspot.com",
+  messagingSenderId: "670329321676",
+  appId: "1:670329321676:web:a2e5423d7c2a1e7602ea69",
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
